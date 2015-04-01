231510043f94ae5011e60ff9f12a6c0f4370f5f3
Shishir Mahajan <shishir.mahajan@redhat.com>
2015-04-01 10:27:17 -0400

    Hacker rank: minimum draws problem
    Signed-off-by: Shishir Mahajan <shishir.mahajan@redhat.com>

---
+++ src/minimum-draws.cpp
@@ -0,0 +1,25 @@
+//Hacker Rank Problems.
+//Solution to minimum draws problem.
+
+//To run the program, please use the test data provided by hacker rank for this problem.
+//https://www.hackerrank.com/challenges/minimum-draws
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int T,N;
+    cin>>T;
+    for(int i=0;i<T;i++)
+        {
+        cin>>N;
+        cout<<N+1<<endl;
+    }
+    return 0;
+}
+
